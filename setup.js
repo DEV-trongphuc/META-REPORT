1db98245c111fc8e6dd035f1ef8272905efd13a5
Huỳnh Trọng Phúc <91412382+DEV-trongphuc@users.noreply.github.com>
2026-02-25 13:15:33 +0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,6 +1,12 @@
 
-let ACCOUNT_ID = "676599667843841";
+let ACCOUNT_ID = "1852318231510577";
 // 📝 Define allowed account IDs here. Leave empty or comment out to show all.
 window.ALLOWED_ACCOUNTS = [
-    "676599667843841", // MBA 577
+    "1852318231510577", // MBA 577
+    "437334141328696", // VTCI 8696
+    "676599667843841", // DOM - 8580
+    "1359762145455986", // IDEAS Institute
+    "631435279612268", // Hub
+    "1046522336367332", // ASC
+    "7426615550731477" // Bún Bò Huế Duyên Mai
 ]; 
